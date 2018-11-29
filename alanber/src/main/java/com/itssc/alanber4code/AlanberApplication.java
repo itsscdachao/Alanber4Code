ceab11e0b503cf9b58a40eebdd632dd4dc5669de
itsscdachao <itsscdachao@gmail.com>
2018-11-29 11:34:09 +0800

    添加com.android.support:multidex:1.0.1

--- alanber/src/main/java/com/itssc/alanber4code/AlanberApplication.java
+++ alanber/src/main/java/com/itssc/alanber4code/AlanberApplication.java
@@ -1,8 +1,16 @@
 package com.itssc.alanber4code;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 public class AlanberApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
