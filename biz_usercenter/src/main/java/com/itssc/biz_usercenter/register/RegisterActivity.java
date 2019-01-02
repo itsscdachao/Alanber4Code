4f98c3fc815b7108c6d1bef34c73fdf46ddcc2f5
itsscdachao <itsscdachao@gmail.com>
2019-01-02 11:28:40 +0800

    2019 new pull 2

---
+++ biz_usercenter/src/main/java/com/itssc/biz_usercenter/register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.itssc.biz_usercenter.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.itssc.biz_usercenter.R;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
