9669795bbb07367b956b08a2eefeacdd72d66fd6
itsscdachao <itsscdachao@gmail.com>
2019-04-09 19:11:58 +0800

    commit

---
+++ tool_flutter/src/main/java/com/itssc/tool_flutter/FlutterTestActivity.java
@@ -0,0 +1,13 @@
+package com.itssc.tool_flutter;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+
+public class FlutterTestActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
